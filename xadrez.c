805ac20a55f82c41d9e8cf65604e3e299f5a2613
bcoelho760 <bcoelho760@gmail.com>
2025-09-16 17:34:01 +0000

    terminei a segunda parte

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int t = 0, b = 0, r = 0;
+    int t = 0, b = 0, r = 0, c = 0;
 
     // movimentação da torre (5 vezes para a direita)
     for (t = 0; t < 5; t++) {
@@ -19,6 +19,16 @@ int main() {
         printf("Esquerda\n");
         r++;
     } while (r <= 8);
+
+    // movimentação do cavalo (2x baixo 1x esquerda)
+    while(c <= 1)
+    {
+        for(c = 0; c <= 1; c++)
+        {
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+    }
 
     return 0;
 }
